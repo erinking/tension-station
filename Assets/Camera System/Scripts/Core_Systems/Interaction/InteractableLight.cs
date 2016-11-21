81f24d852a420b20f13f173c5643ba1a1c60316b
David <David>
2016-11-21 16:33:13 -0500

    Created interactable script for static lamppost

---
+++ Assets/Camera System/Scripts/Core_Systems/Interaction/InteractableLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableLight : InteractableComponent{
+
+	public Light brightLight;
+
+	void Start()
+	{
+		brightLight = GetComponent<Light> ();
+		brightLight.enabled = false;
+	}
+
+	public override void OnInteract()
+	{
+		brightLight.enabled = !brightLight.enabled;
+	}
+}
