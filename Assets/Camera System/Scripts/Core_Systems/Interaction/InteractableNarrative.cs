6d6943f52fd3f88bcf959afe8a2673b9a2bd9561
Loren Sherman <lsherman@mit.edu>
2016-11-22 01:43:53 -0500

    Dumb programmer mistake

---
+++ Assets/Camera System/Scripts/Core_Systems/Interaction/InteractableNarrative.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InteractableNarrative : InteractableComponent{
+
+	public string message;
+	public Text text;
+	public GameObject mask;
+
+	private bool isDisplayingText = false;
+	private bool justInteracted = false;
+
+	void Start(){
+		message = message.Replace ("\\", "\n");
+	}
+
+	void LateUpdate(){
+		if (isDisplayingText && !justInteracted && Input.anyKeyDown) {
+			KillText ();
+			isDisplayingText = false;
+		}
+		justInteracted = false;
+	}
+
+	public override void OnInteract()
+	{
+		if (isDisplayingText) {
+			KillText ();
+		}else{
+			text.text = message;
+			mask.SetActive (true);
+		}
+		isDisplayingText = !isDisplayingText;
+		justInteracted = true;
+	}
+
+	void KillText()
+	{
+		text.text = "";
+		mask.SetActive (false);
+	}
+}
