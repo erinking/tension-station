fdfba794b1ca7a8e0844604bf960865e65d94681
Loren Sherman <lsherman@mit.edu>
2016-12-03 17:43:53 -0500

    Wrote script to dim lantern material when unlit

---
+++ Assets/Camera System/Scripts/Core_Systems/Interaction/LanternMaterialDim.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanternMaterialDim : MonoBehaviour {
+
+	public Light controlLight;
+
+	private Renderer rend;
+	private Color litColor;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		litColor = rend.material.GetColor ("_EmissionColor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color col = Color.Lerp (rend.material.GetColor ("_EmissionColor"), controlLight.enabled ? litColor : Color.black, 0.2f);
+		rend.material.SetColor ("_EmissionColor", col);
+	}
+}
