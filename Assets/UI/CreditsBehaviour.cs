588b348d82ff6f3ae922d1d943a5fe096b89ac58
Loren Sherman <lsherman@mit.edu>
2016-12-12 16:52:00 -0500

    Massive menu screen commit

---
+++ Assets/UI/CreditsBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CreditsBehaviour : MonoBehaviour {
+
+	public RawImage mask;
+	public Text text;
+	public string nextLevel;
+	public float relativeFontSize = 0.03f;
+
+	private float fadeTime = 0.5f;
+	private float currentFade = 1.5f;
+	private bool fadeOut = false;
+
+	// Use this for initialization
+	void Start () {
+		text.fontSize = (int)(Screen.height * relativeFontSize);
+		mask.color = Color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.transform.Translate (0, -Input.GetAxis ("Vertical") * Time.deltaTime * 200, 0);
+
+		if (fadeOut) {
+			currentFade += Time.deltaTime / fadeTime;
+			if (currentFade >= 1) {
+				Application.LoadLevel (nextLevel);
+			}
+		} else if(currentFade > 0) {
+			currentFade -= Time.deltaTime / fadeTime;
+		}
+		mask.color = new Color (0, 0, 0, currentFade);
+
+		if (Input.GetButtonDown ("Interact")) {
+			fadeOut = true;
+		}
+	}
+}
---
+++ Assets/UI/Menu/Menu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public Transform[] menuTargets;
+	public Transform aCircle;
+	public Transform light;
+
+	private int currentTarget = 0;
+	private float timeBuffer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vert = Input.GetAxis ("Vertical");
+		if (vert == 0) {
+			timeBuffer = 0;
+		}
+		if (timeBuffer > 0) {
+			timeBuffer -= Time.deltaTime;
+		} else {
+			if (vert != 0) {
+				timeBuffer = 0.2f;
+				currentTarget += vert < 0 ? 1 : menuTargets.Length - 1;
+				currentTarget = currentTarget % menuTargets.Length;
+				menuUpdate ();
+			}
+		}
+		if (Input.GetButtonDown ("Interact")) {
+			menuAction ();
+		}
+	}
+
+	void menuUpdate(){
+		aCircle.position = menuTargets [currentTarget].position;
+		light.position = aCircle.position + new Vector3 (-3, 1, 0);
+	}
+
+	void menuAction(){
+		switch (currentTarget) {
+		case 0:
+			Application.LoadLevel ("Intro");
+			break;
+		case 1:
+			Application.LoadLevel ("Credits");
+			break;
+		case 2:
+			Application.Quit ();
+			break;
+		}
+	}
+}
